e5146749209094527fb22e88874a2715473897ab
aldillon09 <aldillon09@gmail.com>
2025-01-03 19:22:09 +0100

    script mods

--- scripts/process-csv.py
+++ scripts/process-csv.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 import re
 
@@ -29,7 +30,7 @@ meta_files = {file[13:15]: file for file in files if re.match(pattern=meta_patte
 
 # Load the list of variables required for the analysis.
 variables_to_analyze = pd.read_csv(f"{folder_path}/reference/variables_to_analyze.csv")
-
+variables_for_conversion = pd.read_csv(f"{folder_path}/reference/vars_for_currency_conversion.csv")
 
 for data_filename in data_files:
 
@@ -43,8 +44,32 @@ for data_filename in data_files:
 		data_df = pd.read_csv(data_path, delimiter=';')
 		meta_df = pd.read_csv(meta_path, delimiter=';', usecols=lambda col: col not in cols_to_drop)
 		merged_df = pd.merge(left=data_df, right=meta_df, on=['country', 'variable'], suffixes=('', '_x'))
-		final_df = merged_df[merged_df['variable'].isin(variables_to_analyze['variable'])]
-		final_df.to_csv(os.path.join(processed_path, f"{country_code}.csv"), index=False)
+
+		# Filter by variables required for the analysis.
+		filtered_df = merged_df[merged_df['variable'].isin(variables_to_analyze['variable'])].copy()
+
+		if filtered_df.shape[0] > 0:
+
+			# Create a mapping from 'year' to 'local_currency_per_usd' / 'ppp_conversion_factor_usd'
+			ppp = filtered_df[filtered_df['variable'] == 'xlcuspi999'][['year', 'value']].copy()
+			fx = filtered_df[filtered_df['variable'] == 'xlcusxi999'][['year', 'value']].copy()
+			ppp_map = ppp.set_index('year')['value'].to_dict()
+			fx_map = fx.set_index('year')['value'].to_dict()
+
+			# Add columns for converted values - PPP and USD
+			filtered_df.loc[:, 'value_usd'] = np.where(
+				filtered_df['variable'].isin(variables_for_conversion['variable']),
+				filtered_df['value'] / filtered_df['year'].map(fx_map),
+				filtered_df['value']
+			)
+
+			filtered_df.loc[:, 'value_ppp'] = np.where(
+				filtered_df['variable'].isin(variables_for_conversion['variable']),
+				filtered_df['value'] / filtered_df['year'].map(fx_map),
+				filtered_df['value']
+			)
+
+			filtered_df.to_csv(os.path.join(processed_path, f"{country_code}.csv"), index=False)
 
 
 print('Task completed successfully.')
