473fb1f15e88e77c555d6fb330aab201094e9c80
aldillon09 <aldillon09@gmail.com>
2025-01-06 16:29:11 +0100

    notebook updates

--- scripts/process-csv.py
+++ scripts/process-csv.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import os
 import re
+import time
+import json
+
+start_time = time.time()
 
 # Define file paths.
 script_dir = os.path.dirname(__file__)  
@@ -31,20 +35,25 @@ meta_files = {file[13:15]: file for file in files if re.match(pattern=meta_patte
 # Load the list of variables required for the analysis.
 variables_to_analyze = pd.read_csv(f"{folder_path}/reference/variables_to_analyze.csv")
 variables_for_conversion = pd.read_csv(f"{folder_path}/reference/vars_for_currency_conversion.csv")
-variables_for_conversion_avg = pd.read_csv(f"{folder_path}/reference/vars_for_currency_conversion_avg.csv")
-variables_for_conversion_total = pd.read_csv(f"{folder_path}/reference/vars_for_currency_conversion_total.csv")
+#variables_for_conversion_avg = pd.read_csv(f"{folder_path}/reference/vars_for_currency_conversion_avg.csv")
+#variables_for_conversion_total = pd.read_csv(f"{folder_path}/reference/vars_for_currency_conversion_total.csv")
 
-print('Are all variables from total csv in aggregate csv?')
-print(np.all(np.isin(variables_for_conversion_total['variable'], variables_for_conversion['variable'])))
-print('Are all variables from avg csv in aggregate csv?')
-print(np.all(np.isin(variables_for_conversion_avg['variable'], variables_for_conversion['variable'])))
+with open(f"{folder_path}/reference/public_spending_vars.json", 'r') as file:
+    public_spending_vars = json.load(file)
+
+public_spending_total = public_spending_vars['Total']
+public_spending_per_capita = public_spending_vars['Average']
+public_spending_pct_of_income = public_spending_vars['Wealth-income ratio']
+ps_pc_pct_combined = public_spending_per_capita + public_spending_pct_of_income
+
+total_rows = 0
 
 for data_filename in data_files:
 
 	# Isolate the country code for each file, eg. US.
 	country_code = data_filename[9:11]
 
-	if country_code in meta_files and country_code == 'US':
+	if country_code in meta_files:# and country_code == 'AU':
 
 		data_path = os.path.join(unprocessed_path, data_filename)
 		meta_path = os.path.join(unprocessed_path, meta_files[country_code])
@@ -63,10 +72,6 @@ for data_filename in data_files:
 			fx = filtered_df[filtered_df['variable'] == 'xlcusxi999'][['year', 'value']].copy()
 			fx_map = fx.set_index('year')['value'].to_dict()
 
-			# Create a mapping to add per capita values to the rows containing totals (in currency only).
-			
-			
-
 			# Add columns for converted values - PPP and USD
 			filtered_df.loc[:, 'value_usd'] = np.where(
 				filtered_df['variable'].isin(variables_for_conversion['variable']),
@@ -80,9 +85,23 @@ for data_filename in data_files:
 				None
 			)
 
-			# 
+			# Create a mapping to add per capita values to the rows containing totals (in currency only).
+			per_capita = filtered_df[filtered_df['variable'].isin(public_spending_per_capita)][['year', 'shortname', 'shortage', 'value_usd']]
+			per_capita_map = per_capita.set_index(['year', 'shortname', 'shortage'])['value_usd'].to_dict()
 
-			filtered_df.to_csv(os.path.join(processed_path, f"{country_code}.csv"), index=False)
+			filtered_df['key'] = list(zip(filtered_df['year'], filtered_df['shortname'], filtered_df['shortage']))
+			filtered_df['value_usd_per_capita'] = filtered_df['key'].map(per_capita_map).where(
+				filtered_df['variable'].isin(public_spending_total)
+			)
 
+			filtered_df_trimmed = filtered_df[~filtered_df['variable'].isin(ps_pc_pct_combined)]
+			
+			filtered_df_trimmed.to_csv(os.path.join(processed_path, f"{country_code}.csv"), index=False)
 
+			total_rows += filtered_df_trimmed.shape[0]
+
+end_time = time.time()
+
+print(f"Execution time: {end_time - start_time:.4f} seconds")
+print(f"Total number of rows processed: {total_rows}")
 print('Task completed successfully.')
