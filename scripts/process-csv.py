c6edc1771e344fe251aed4dfc80cd46adba794cf
aldillon09 <aldillon09@gmail.com>
2025-01-08 17:18:51 +0100

    script and notebook updates

--- scripts/process-csv.py
+++ scripts/process-csv.py
@@ -41,7 +41,7 @@ variables_to_analyze = pd.read_csv(f"{folder_path}/reference/variables_to_analyz
 variables_for_conversion = pd.read_csv(f"{folder_path}/reference/vars_for_currency_conversion.csv")
 
 # Load region data for each country.
-region_data = pd.read_csv(f"{folder_path}/reference/WID_countries.csv", delimiter=';', index_col='alpha2')[['region', 'region2']]
+region_data = pd.read_csv(f"{folder_path}/reference/WID_countries.csv", delimiter=';', index_col='alpha2')[['region', 'region2']].dropna(subset=['region'])
 region_map = region_data['region'].to_dict()
 subregion_map = region_data['region2'].to_dict()
 
@@ -59,6 +59,7 @@ step1 = 0
 step2 = 0
 step3 = 0
 step4 = 0
+step5 = 0
 
 for data_filename in data_files:
 	step1 += 1
@@ -105,29 +106,33 @@ for data_filename in data_files:
 				filtered_df['region'] = filtered_df['country'].map(region_map)
 				filtered_df['subregion'] = filtered_df['country'].map(subregion_map)
 
-				# Create a mapping to add per capita values to the rows containing totals (in currency only).
-				per_capita = filtered_df[filtered_df['variable'].isin(public_spending_per_capita)][['year', 'shortname', 'shortage', 'value_usd']]
-				per_capita_map = per_capita.set_index(['year', 'shortname', 'shortage'])['value_usd'].to_dict()
+				if not filtered_df['region'].isna().all():
 
-				filtered_df['key'] = list(zip(filtered_df['year'], filtered_df['shortname'], filtered_df['shortage']))
-				filtered_df['value_usd_per_capita'] = filtered_df['key'].map(per_capita_map).where(
-					filtered_df['variable'].isin(public_spending_total)
-				)
+					step5 += 1
 
-				# Create a mapping to add per capita values to the rows containing totals (in currency only).
-				pct_of_income = filtered_df[filtered_df['variable'].isin(public_spending_pct_of_income)][['year', 'shortname', 'shortage', 'value']]
-				pct_of_income_map = pct_of_income.set_index(['year', 'shortname', 'shortage'])['value'].to_dict()
+					# Create a mapping to add per capita values to the rows containing totals (in currency only).
+					per_capita = filtered_df[filtered_df['variable'].isin(public_spending_per_capita)][['year', 'shortname', 'shortage', 'value_usd']]
+					per_capita_map = per_capita.set_index(['year', 'shortname', 'shortage'])['value_usd'].to_dict()
 
-				filtered_df['key'] = list(zip(filtered_df['year'], filtered_df['shortname'], filtered_df['shortage']))
-				filtered_df['value_pct_national_income'] = filtered_df['key'].map(pct_of_income_map).where(
-					filtered_df['variable'].isin(public_spending_total)
-				)
+					filtered_df['key'] = list(zip(filtered_df['year'], filtered_df['shortname'], filtered_df['shortage']))
+					filtered_df['value_usd_per_capita'] = filtered_df['key'].map(per_capita_map).where(
+						filtered_df['variable'].isin(public_spending_total)
+					)
 
-				filtered_df_trimmed = filtered_df[~filtered_df['variable'].isin(ps_pc_pct_combined)].drop(columns=['key', 'percentile'])
-				
-				filtered_df_trimmed.to_csv(os.path.join(processed_path, f"{country_code}.csv"), index=False)
+					# Create a mapping to add per capita values to the rows containing totals (in currency only).
+					pct_of_income = filtered_df[filtered_df['variable'].isin(public_spending_pct_of_income)][['year', 'shortname', 'shortage', 'value']]
+					pct_of_income_map = pct_of_income.set_index(['year', 'shortname', 'shortage'])['value'].to_dict()
 
-				total_rows += filtered_df_trimmed.shape[0]
+					filtered_df['key'] = list(zip(filtered_df['year'], filtered_df['shortname'], filtered_df['shortage']))
+					filtered_df['value_pct_national_income'] = filtered_df['key'].map(pct_of_income_map).where(
+						filtered_df['variable'].isin(public_spending_total)
+					)
+
+					filtered_df_trimmed = filtered_df[~filtered_df['variable'].isin(ps_pc_pct_combined)].drop(columns=['key', 'percentile'])
+					
+					filtered_df_trimmed.to_csv(os.path.join(processed_path, f"{country_code}.csv"), index=False)
+
+					total_rows += filtered_df_trimmed.shape[0]
 				
 
 end_time = time.time()
@@ -138,4 +143,5 @@ print(f"No. of countries left after step 1: {step1}")
 print(f"No. of countries left after step 2: {step2}")
 print(f"No. of countries left after step 3: {step3}")
 print(f"No. of countries left after step 4: {step4}")
+print(f"No. of countries left after step 4: {step5}")
 print('Task completed successfully.')
